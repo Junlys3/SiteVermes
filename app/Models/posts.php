f9b2c43c52c8132b3520a0994379d5749d31c201
Júlio <juliosdelima1@gmail.com>
2025-07-15 13:08:57 -0300

    bug fix 3

--- app/Models/posts.php
+++ app/Models/posts.php
@@ -20,4 +20,10 @@ class posts extends Model
     {
         return $this->belongsTo(User::class, 'id_user');
     }
+
+   public function comments()
+  {
+    return $this->hasMany(CommentsPost::class, 'id_post');
+  }  
+
 }
