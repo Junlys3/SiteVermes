c2e1b4018536dfaaa933016b5c69487971c91d3f
Júlio <125740363+Junlys3@users.noreply.github.com>
2025-07-15 11:31:14 -0300

    migrations, models comments

---
+++ app/Models/CommentsPost.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class CommentsPost extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'text',
+        'id_user',
+        'id_post'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user');
+    }
+
+    public function post()
+    {
+        return $this->belongsTo(posts::class, 'id_post');
+    }
+}
